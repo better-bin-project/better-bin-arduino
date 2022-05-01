9e5784613f206957ee5c8a524a2ed0311f2008c8
Finn Rudolph <finn.rudolph@t-online.de>
2022-05-01 13:50:07 +0200

    Add function for reading RGB sensor

--- better-bin-arduino.ino
+++ better-bin-arduino.ino
@@ -1,33 +1,63 @@
-#define UV '1'
-#define INFRARED '2'
+#include <Wire.h>
+#include "Adafruit_TCS34725.h"
 
-int readUV();
+#define UV '1'
+#define RGB '2'
+
+Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_614MS, TCS34725_GAIN_1X);
+
+typedef struct rgbData rgbData;
+struct rgbData {
+  uint16_t r;
+  uint16_t g;
+  uint16_t b;
+  uint16_t c;
+};
+
+unsigned long readUV();
+void readRGB(rgbData* data);
 
 void setup() {
     Serial.begin(9600);
+    if (!tcs.begin()) {
+      Serial.write("Color Sensor not found.");
+    } else {
+      Serial.write("Color Sensor connected.");
+    }
 }
 
 void loop() {
     if (Serial.available() > 0) {
         char msg = Serial.readStringUntil('\n')[0];
+        msg = RGB;
         // Read and send back requested sensor data
         switch (msg) {
             case UV:
                 // todo: UV light on
-                Serial.print(readUV());
+                Serial.write(readUV());
                 // todo: UV light off
                 break;
-            case INFRARED:
+            case RGB:
+                rgbData data;
+                readRGB(&data);
+                Serial.write(data.r);
+                Serial.write(data.g);
+                Serial.write(data.b);
+                Serial.write(data.c);
                 break;
         }
     }
 }
 
-int readUV() {
-    int val = 0;
-    for (int i = 0; i < 1024; i++) {
+unsigned long readUV() {
+    unsigned long val = 0;
+    for (size_t i = 0; i < 1024; i++) {
         val += analogRead(A0);
         delay(2);
     }
-    return val / 1024; // todo: Return percentage of maximum intensity
+    return val / 1024; // todo: Return percentage of maximum intensity or [0, 255]
+}
+
+void readRGB(rgbData* data) {
+    tcs.getRawData(&data->r, &data->g, &data->b, &data->c);
 }
